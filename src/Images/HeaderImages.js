9279d4e84d93464478da0562dc905a5da227def7
Alexander Bogdanov <alxbgdnv2002@gmail.com>
2024-08-06 23:48:03 +0800

    Update

--- src/Images/HeaderImages.js
+++
@@ -1,7 +0,0 @@
-// images.js
-import logo from './Logo.png';
-import heart from './Heart.png';
-import notifications from './Notifications.png';
-import userIcon from './User_icon.png';
-
-export { logo, heart, notifications, userIcon };
